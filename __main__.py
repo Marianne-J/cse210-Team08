a73e550df521a2740f43db6e36952ea649ef8a3a
Marianne Johnson <joh20041@byui.edu>
2021-10-01 20:10:39 -0700

    Another minor update to __main__.py

--- __main__.py
+++ __main__.py
@@ -1,12 +1,8 @@
 #Import greeting functions
 from greetings.sanders import display_stephen
-<<<<<<< HEAD
 from greetings.johnson import greeting
-
-=======
-from mjgreeting.johnson import greeting
 from greetings.taylor import taylor_greeting
->>>>>>> 4402e3c4ca028e70d74e4d8e2466191d17386350
+
 #Intro
 print('Welcome to our collaborative program.')
 
