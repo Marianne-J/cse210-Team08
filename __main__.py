4402e3c4ca028e70d74e4d8e2466191d17386350
Ptaylo11 <91767160+Ptaylo11@users.noreply.github.com>
2021-10-01 19:59:10 -0600

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,10 +1,11 @@
 #Import greeting functions
 from greetings.sanders import display_stephen
 from mjgreeting.johnson import greeting
-
+from greetings.taylor import taylor_greeting
 #Intro
 print('Welcome to our collaborative program.')
 
 #Functions
 display_stephen()
 greeting()
+taylor_greeting()
